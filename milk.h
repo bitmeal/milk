c0910f4fc8df9f776e530ad3cf21d8a32c5d4193
Arne Wendt <arne.wendt@tu-harburg.de>
2019-02-14 13:21:09 +0100

    #pragma one, cleanup

--- milk.h
+++ milk.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <typeinfo>
 #include <type_traits>
 
@@ -40,7 +42,6 @@ namespace milk
 	typedef bite_member_proxy_base<milk::bite> bite_member_proxy;
 	typedef grain_base<milk::bite> grain;
 	typedef bite_iterator_base<milk::bite, milk::grain> bite_iterator;
-	//typedef bite_iterator_base<milk::bite, milk::grain>::iterator_type_t bite_iterator::iterator_type_t;
 
 }
 
@@ -55,69 +56,7 @@ namespace milk
 namespace milk
 {
 
-	// MEMBER_PROXY
-	/*
-	template<>
-	template<>
-	milk::bite& bite_member_proxy_base<milk::bite>::operator = <milk::bite>(const milk::bite& other)
-	{
-		if (this->grain != other.grain)
-		{
-			if (this->grain)
-				*(this->grain) = milk::grain_base<milk::bite>(*(other.grain)); // should already set correct data if grain != 0 !? (grain is shared pointer)
-			else
-			{
-				//set(other);
-				this->grain = other.grain; // should already set correct data if grain != 0 !? (grain is shared pointer)
-									 //
-				parent.mutable_idx(key) = *this;
-			}
-		}
-
-		return (milk::bite)*this;
-	};
-
-	template<>
-	template<>
-	milk::bite& bite_member_proxy_base<milk::bite>::operator = <milk::bite>(const milk::bite_member_proxy& other)
-	{
-		if (this->grain != other.grain)
-		{
-			if (grain)
-				*(this->grain) = milk::grain_base<milk::bite>(*(other.grain)); // should already set correct data if grain != 0 !? (grain is shared pointer)
-			else
-			{
-				//set(other);
-				this->grain = other.grain; // should already set correct data if grain != 0 !? (grain is shared pointer)
-									 //
-				parent.mutable_idx(key) = *this;
-			}
-		}
-
-		return (milk::bite)*this;
-	};
-	*/
-
-	/*
-	// GRAIN
-	template<>
-	template<>
-	grain_base<milk::bite>::grain_base<std::string>(const std::string& val) : grain_base()
-	{
-		type = n_str;
-		d_str_bin = std::make_unique<str_bin_t>(val.begin(), val.end());
-	};
-
-	template<>
-	template<>
-	grain_base<milk::bite>::grain_base<bool>(const bool& val) : grain_base()
-	{
-		type = s_bool;
-		scalar_data.d_bool = val;
-	};
-	*/
-
-	// ITERATOR value
+	// ITERATOR value operator = overload
 	template<>
 	template<>
 	milk::bite& bite_iterator_value<milk::bite>::operator = <milk::bite>(const milk::bite& other)
@@ -126,6 +65,5 @@ namespace milk
 		*(this->grain) = *(other.grain);
 		return *this;
 	};
-
 
 }
